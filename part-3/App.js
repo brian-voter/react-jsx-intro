6f614a35ac7ca06890bee52838a3fa44b728332a
John Chong <johnchong.99.17@gmail.com>
2023-05-22 14:21:42 -0500

    implement part 3

---
+++ part-3/App.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function App() {
+  return (
+  <div>
+    <Person name="Tarzan" age={23} hobbies={["swinging", "yelling"]} />
+    <Person name="Jane" age={17} hobbies={["reading", "screaming"]} />
+  </div>
+  );
+}
---
+++ part-3/Person.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function Person({ name, age, hobbies = [] }) {
+  const tweetStyle = {
+    border: '1px solid black',
+    margin: '10px',
+    padding: '5px',
+    backgroundColor: 'blue',
+    color: 'white',
+    width: '50%'
+  };
+  let header3Text = age > 18 ? "please go vote!" : "you must be 18";
+
+  return <div style={tweetStyle}>
+    <h3>{header3Text}</h3>
+    <p>Learn some information about this person <br />
+      <b>name: {name.slice(8)} ({age})</b></p>
+    <h4>Hobbies:</h4>
+    <ul>
+      {hobbies.map(hobby => <li>{hobby}</li>)}
+    </ul>
+  </div>;
+}
